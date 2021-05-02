b6ef933b60609b2e0995f833d23a330ebec0c00e
Ruhana006 <ritykarim33616@gmail.com>
2021-05-02 09:44:27 +0600

    blog site link added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,4 +1,5 @@
 import { faMedium } from '@fortawesome/free-brands-svg-icons';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import Typed from 'react-typed'
@@ -9,6 +10,7 @@ const Blogs = () => {
         <div id="blogs" className="mt-5 mb-5">
             <div style={{ textAlign: 'center' }} className="bg-dark text-white p-2 mb-5 mt-5">
                 <h1 className="display-5">Personal Blogs</h1>
+                <a className="icon" href="https://admin.digitalpress.blog/a" target="_blank"><FontAwesomeIcon size="2x" icon={faEdit}/><h5>Visit My Blog Site</h5></a>
                 <div className="mt-5 mb-5 p-5">
                     <Typed style={{ fontFamily: "italic" }} className="display-4" strings={['Blogs Are Coming Soon']} typeSpeed={60} backSpeed={60} loop></Typed>
                 </div>
